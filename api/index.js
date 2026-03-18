b28a810353f8ab9f0f684f3c20cd3dc076d5ad7a
Kai Summer <kavindug1998@gmail.com>
2026-03-18 19:52:52 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,5 +2,5 @@
  * Vercel Serverless Entry Point
  * Re-exports the Express app for Vercel's serverless function runtime.
  */
-import app from '../server/index.js';
+import app from './server/index.js';
 export default app;
